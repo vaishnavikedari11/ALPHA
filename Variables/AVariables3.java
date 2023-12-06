f1f2b9b5df71cd6a2816fd005de2a145b74ad9d3
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-06 15:56:02 +0530

    Add files via upload

---
+++ Variables/AVariables3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AVariables3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter radius of circle" );
+        int radius=sc.nextInt();
+        double area=3.14 *radius*radius;
+        System.out.println(" area of circle "+area);
+        
+
+
+
+
+    }
+}
---
+++ Variables/AVariables4.java
@@ -0,0 +1,5 @@
+public class AVariables4 {
+    public static void main(String[] args) {
+        
+    }
+}
