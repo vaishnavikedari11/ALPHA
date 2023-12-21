a6ea5f36b31d1ecfb50d093fb3deea74afb5d53e
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2023-12-21 13:18:55 +0530

    Add files via upload

---
+++ Arrays/FArrays4.java
@@ -0,0 +1,26 @@
+//binary search---sorted array given
+public class FArrays4 {
+    public static int binarySearch(int numbers[],int key) {
+        int start=0 ;
+        int end=numbers.length-1;
+
+        while(start<=end){
+            int mid=(start+end)/2;
+                //composition
+                if(numbers[mid]<=key){
+                    return mid;
+                }else{
+                    end=mid-1;
+                }
+            }
+        
+        return -1;
+        
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10,12,14};
+        int key=4;
+
+        System.out.println("index for key "+binarySearch(numbers, key) );
+    }
+}
